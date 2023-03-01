d5830f4922f4ae265d20a258ce4b48b17a56834d
benjimoon <bjm375@drexel.edu>
2023-03-01 17:57:41 -0500

    hw04

--- idm362-bjm375/ViewController.swift
+++ idm362-bjm375/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
 }
 
